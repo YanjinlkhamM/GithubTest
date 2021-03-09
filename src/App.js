1c9b50ff2121736afeca889e681d8722724c778a
Yanjinlkham <yanjka@iteyes.co.kr>
2021-03-09 12:25:17 +0800

    useState with previous state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
+import HookCountertwo from './components/HookCountertwo';
 
 function App() {
   return (
     <div className="App">
    {/* <ClassCounter/>*/}
-   <HookCounter></HookCounter> 
+  {/*<HookCounter></HookCounter>*/ }  
+    <HookCountertwo></HookCountertwo>
     </div>
   );
 }
